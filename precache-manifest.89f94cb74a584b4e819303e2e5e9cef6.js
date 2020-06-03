75d86c99da263ae95925e968b72e74333d39da5e
kenson <pkensonpandey@gmail.com>
2020-06-03 08:10:07 +0545

    Updates

---
+++ precache-manifest.89f94cb74a584b4e819303e2e5e9cef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c702470260c056504b26f402707b99",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "ced766ff1fcff656b9a8",
+    "url": "/ReactProject/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8a9682b7101e9b512a1a",
+    "url": "/ReactProject/static/css/main.a0e3ff5a.chunk.css"
+  },
+  {
+    "revision": "ced766ff1fcff656b9a8",
+    "url": "/ReactProject/static/js/2.47845ee5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ReactProject/static/js/2.47845ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9682b7101e9b512a1a",
+    "url": "/ReactProject/static/js/main.cd68ba3c.chunk.js"
+  },
+  {
+    "revision": "c60b497f8175ec3c529f",
+    "url": "/ReactProject/static/js/runtime-main.403ab457.js"
+  }
+]);
